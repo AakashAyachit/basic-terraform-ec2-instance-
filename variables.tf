02420ab7491549b02286f6aa5383b369c36a3900
Aakash Ayachit <110335913+AakashAyachit@users.noreply.github.com>
2023-02-21 12:24:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
-#Name = var.instance_name  <-- use this in Tags in main.tf
+#Name = var.instance_name  <-- use this in tags in main.tf
 variable "instance_name" {
     description = "name of ec2 instance"
     type = string
